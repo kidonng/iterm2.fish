094d1e2b7b044047a49d8177c657bf5e67c76231
Kid <kidonng@gmail.com>
2021-09-09 00:04:58 +0800

    Style tweaks

--- conf.d/iterm2.fish
+++ conf.d/iterm2.fish
@@ -1,8 +1,8 @@
-if ! status -i || test "$ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX""$TERM" = screen || test "$TERM" = dumb || test "$TERM" = linux
+if ! status -i || test "$ITERM_ENABLE_SHELL_INTEGRATION_WITH_TMUX$TERM" = screen || test "$TERM" = dumb || test "$TERM" = linux
     exit
 end
 
-function iterm2_preexec --on-event fish_preexec
+function iterm2_preexec -e fish_preexec
     # For other shells we would output status here but we can't do that in fish.
     printf "\033]133;C;\007"
 end
@@ -13,12 +13,12 @@ if test (iterm2_has_fish_mode_prompt) = true
     # Only override fish_mode_prompt if it is non-empty. This works around a problem created by a
     # workaround in starship: https://github.com/starship/starship/issues/1283
     functions -c fish_mode_prompt iterm2_fish_mode_prompt
-    function fish_mode_prompt --description 'Write out the mode prompt; do not replace this. Instead, change fish_mode_prompt before sourcing .iterm2_shell_integration.fish, or modify iterm2_fish_mode_prompt instead.'
+    function fish_mode_prompt -d 'Write out the mode prompt; do not replace this. Instead, change fish_mode_prompt before sourcing .iterm2_shell_integration.fish, or modify iterm2_fish_mode_prompt instead.'
         iterm2_common_prompt
         iterm2_fish_mode_prompt
     end
 
-    function fish_prompt --description 'Write out the prompt; do not replace this. Instead, change fish_prompt before sourcing .iterm2_shell_integration.fish, or modify iterm2_fish_prompt instead.'
+    function fish_prompt -d 'Write out the prompt; do not replace this. Instead, change fish_prompt before sourcing .iterm2_shell_integration.fish, or modify iterm2_fish_prompt instead.'
         # Remove the trailing newline from the original prompt. This is done
         # using the string builtin from fish, but to make sure any escape codes
         # are correctly interpreted, use %b for printf.
@@ -28,7 +28,7 @@ if test (iterm2_has_fish_mode_prompt) = true
     end
 else
     # fish_mode_prompt is empty or unset.
-    function fish_prompt --description 'Write out the mode prompt; do not replace this. Instead, change fish_mode_prompt before sourcing .iterm2_shell_integration.fish, or modify iterm2_fish_mode_prompt instead.'
+    function fish_prompt -d 'Write out the mode prompt; do not replace this. Instead, change fish_mode_prompt before sourcing .iterm2_shell_integration.fish, or modify iterm2_fish_mode_prompt instead.'
         iterm2_common_prompt
 
         # Remove the trailing newline from the original prompt. This is done
@@ -44,7 +44,7 @@ end
 if not set -q -g iterm2_hostname
     # hostname -f is fast on macOS so don't cache it. This lets us get an updated version when
     # it changes, such as if you attach to a VPN.
-    if [ (uname) != Darwin ]
+    if test (uname) != Darwin
         set -g iterm2_hostname (hostname -f 2>/dev/null)
         # some flavors of BSD (i.e. NetBSD and OpenBSD) don't have the -f option
         if test $status -ne 0
